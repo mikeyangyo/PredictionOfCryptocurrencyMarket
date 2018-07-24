de6278d0128a691608c95c9892fa9e2f9e19a46b
mikeyang <perryvm06vm06@gmail.com>
2018-07-24 21:09:40 +0800

    remove crawler file and add a notebook to test api provided by reddit

--- crawler for reddit/reddit/models.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from scrapy.utils.project import get_project_settings
-
-def db_connect():
-    settings = get_project_settings()
-    connection = MongoClient(settings['MONGODB_SERVER'],settings['MONGODB_PORT'])
-    collection = connection[settings['MONGODB_DB']][settings['MONGODB_COLLECTION']]
-    return collection
